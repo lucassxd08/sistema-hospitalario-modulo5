a4bd70daaafcef8b807689566f487be57daef18e
Lucas Inga <lucas.inga@tecsup.edu.pe>
2026-09-11 17:26:48 -0500

    Setup inicial del proyecto Spring Boot

---
+++ src/test/java/com/hospital/historia_clinica/HistoriaClinicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hospital.historia_clinica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HistoriaClinicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
